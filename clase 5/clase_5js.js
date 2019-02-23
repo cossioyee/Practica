be61b5f6b8f723de51bd2e479b152e7ffee6a35d
cossioyee <cossioyee@yahoo.es>
2019-02-23 11:43:09 -0500

    estoy tratando de que dibuje con el mouse

--- clase 5/clase_5js.js
+++ clase 5/clase_5js.js
@@ -7,56 +7,28 @@ var teclas = {
 };
 
 //Creacion de lienzo, posicion inicial y declaracon del evento
-document.addEventListener("keydown", dibujarTeclado);
-var cuadrito = document.getElementById("canva");
-var papel = cuadrito.getContext("2d");
-var x = 100;
-var y = 100;
-dibujar_linea("green",x-1,y-1,x+1,y+1,papel)
-//funcion de dibujo
-function dibujar_linea(color, xini, yini, xfin, yfin, lienzo)
-  {
-    lienzo.beginPath();
-    lienzo.strokeStyle = color;
-    lienzo.lineWidth = 3;
-    lienzo.moveTo(xini, yini);
-    lienzo.lineTo(xfin, yfin);
-    lienzo.stroke();
-    lienzo.closePath();
-  }
+document.addEventListener("mousedown", dibujarTeclado);
 
 //funcion de movimiento
 function dibujarTeclado(evento)
 {
-  var colorcito = "BLUE";
-  var movimiento = 1;
-  switch(evento.keyCode)
-  {
-    case teclas.UP:
-      dibujar_linea("colorcito",x,y,x,y - movimiento, papel);
-      y = y -movimiento;
-    break;
+ 	var colorcito = "BLUE";
+ 	var movimiento = 1;
+ 	var cuadrito = document.getElementById("canva");
+	var papel = cuadrito.getContext("2d");
+	var x = evento.layerX;
+	var y = evento.layerY;
+	dibujar_linea("green",x,y,x+1,y+1,papel)
+	//funcion de dibujo
+	function dibujar_linea(color, xini, yini, xfin, yfin, lienzo)
+  		{
+    		lienzo.beginPath();
+    		lienzo.strokeStyle = color;
+    		lienzo.lineWidth = 3;
+    		lienzo.moveTo(xini, yini);
+   			lienzo.lineTo(xfin, yfin);
+    		lienzo.stroke();
+    		lienzo.closePath();
+  		}
 
-    case teclas.DOWN:
-      console.log("Vamos para abajo");
-      dibujar_linea("colorcito",x,y,x,y + movimiento, papel);
-      y = y + movimiento;
-    break;
-
-    case teclas.RIGTH:
-      console.log("Vamos para arriba");
-      dibujar_linea("colorcito",x,y,x - movimiento,y, papel);
-      x = x - movimiento;
-    break;
-
-    case teclas.LEFT:
-      console.log("Vamos para abajo");
-      dibujar_linea("colorcito",x,y,x + movimiento,y, papel);
-      x = x + movimiento;
-    break;
-    
-    default:
-      console.log("Otra tecla");
-    break;
-  }
 }
