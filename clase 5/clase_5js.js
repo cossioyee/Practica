e97ffb5909c3af5e062e2182a553a15c5c40bd4d
cossioyee <cossioyee16@yahoo.es>
2019-02-24 13:08:53 -0500

    este es un commit anter de verificar el github online

--- clase 5/clase_5js.js
+++ clase 5/clase_5js.js
@@ -1,34 +1,43 @@
 //Declaracion de codigo de teclas
 var teclas = {
-  UP: 38,
-  DOWN: 40,
-  RIGTH: 37,
-  LEFT: 39,
+	UP: 38,
+	DOWN: 40,
+	RIGTH: 37,
+	LEFT: 39,
 };
 
-//Creacion de lienzo, posicion inicial y declaracon del evento
-document.addEventListener("mousedown", dibujarTeclado);
+//Declaracon del evento
+document.addEventListener("mousemove", dibujarTeclado);
 
 //funcion de movimiento
 function dibujarTeclado(evento)
 {
  	var colorcito = "BLUE";
- 	var movimiento = 1;
  	var cuadrito = document.getElementById("canva");
 	var papel = cuadrito.getContext("2d");
-	var x = evento.layerX;
-	var y = evento.layerY;
-	dibujar_linea("green",x,y,x+1,y+1,papel)
+	
+	if (evento.buttons === 1)
+	{
+		var x = evento.layerX;
+		var y = evento.layerY;
+		dibujar_linea("green",x+3,y+3,x-3,y-3,papel)
+	}
+	
+	
 	//funcion de dibujo
 	function dibujar_linea(color, xini, yini, xfin, yfin, lienzo)
   		{
     		lienzo.beginPath();
     		lienzo.strokeStyle = color;
-    		lienzo.lineWidth = 3;
+    		lienzo.lineWidth = 1;
     		lienzo.moveTo(xini, yini);
    			lienzo.lineTo(xfin, yfin);
     		lienzo.stroke();
-    		lienzo.closePath();
+    		
+  		}
+  	function detener_linea()
+  		{
+  			lienzo.closePath();
   		}
 
 }
